c4364c79c229c1bf06acc6d67408683d2b0b8532
Quinn Meurer <quinnyyy@bu.edu>
2019-02-25 17:54:08 -0500

    Task 2 done

--- ffmpeg.py
+++ ffmpeg.py
@@ -1,11 +1,11 @@
-import threading
 import subprocess
 import os
+import time
+import Queue
 
 def encode(file):
     vid_720p = "ffmpeg -i " + file + " -b:v 2M -r 30 -s hd720 hd720" + file
     vid_480p = "ffmpeg -i " + file + " -b:v 1M -r 30 -s hd480 hd480" + file
-
     print(file + " 720 is starting")
     subprocess.call(vid_720p, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
     print(file + " 720 is finished")
@@ -15,4 +15,7 @@ def encode(file):
     print(file + " 480 is finished")
 
 if __name__ == "__main__":
+    start = time.time()
     encode("bball.mp4")
+    end = time.time()
+    print("It took ", end - start, " s")
--- queuevids.py
+++ queuevids.py
@@ -1,5 +1,28 @@
 import queue
-
+import threading
+import subprocess
+import os
 
 def queue_vids(files, function):
-        
+    q = queue.Queue()
+    for f in files:
+        t = threading.Thread(target=function, args = (f,))
+        t.daemon = True
+        q.put(t)
+
+    while not q.empty():
+        t1 = q.get()
+        t1.start()
+        t2 = q.get()
+        t2.start()
+        t1.join()
+        t2.join()
+
+def encode2(file):
+    vid_720p = "ffmpeg -i " + file + " -b:v 2M -r 30 -s hd720 hd720" + file
+    print(file + " 720 is starting")
+    subprocess.call(vid_720p, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
+    print(file + " 720 is finished")
+
+if __name__ == "__main__":
+    queue_vids(["bball.mp4", "bball2.mp4", "bball3.mp4", "bball4.mp4"],encode2)
